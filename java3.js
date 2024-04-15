0e648c7a7f0f8dacf130a45f3b188151def257e9
gustamartins <130712380+gustamarins@users.noreply.github.com>
2024-04-15 19:59:36 -0300

    Add files via upload

--- java3.js
+++ java3.js
@@ -1,6 +1,10 @@
 mochila = []
 
 function atualizarLista(){
+
+    document.getElementById('capacidade').innerHTML = mochila.length +'/4'
+    document.getElementById('item').value = ''
+
     html = ''
     for(i=0; i<mochila.length; i++){
         html += '<div>'+mochila[i]+' <button onclick="removerIten('+i+')">X</button></div>'
@@ -10,8 +14,21 @@ function atualizarLista(){
 
 function adicionar(){
     item = document.getElementById('item').value
-    mochila.push(item)
-    atualizarLista()
+    item = item[0].toUpperCase() + item.slice(1).toLowerCase()
+
+    if(item == ''){
+        alert('Campo vazio')
+    }else if(mochila.indexOf(item) != -1){
+        alert('Este item já está na lista')
+    }else{
+        if(mochila.length < 4){
+            mochila.push(item)
+            atualizarLista() 
+        }else{
+            alert('A mochila está cheia.')
+        }
+
+    }
 }
 
 function removerIten(i){
